4155c472a8fd8be291da6398a40602dc459f5be5
fakhitah3 <fakhitah.ridzuan@gmail.com>
2024-10-27 09:28:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -15,7 +15,9 @@ POP_SIZE = 500
 MUT_RATE = 0.2
 
 #TARGET: Our goal.
-TARGET = 'fakhitah'
+#TARGET = 'fakhitah'
+TARGET = st.text_input("Enter your name")
+
 
 #GENES: Options from which our population would be created.
 GENES = ' abcdefghijklmnopqrstuvwxyz'
