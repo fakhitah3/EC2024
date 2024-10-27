bd31c63765ffbd007d55245e10163bb8df2c7a14
fakhitah3 <fakhitah.ridzuan@gmail.com>
2024-10-27 09:34:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,12 +9,12 @@ st.header("Genetic Algorithm", divider="gray")
 import random
 
 #POP_SIZE: Number of Chromosomes in our list.
-#POP_SIZE = 500
-POP_SIZE = st.number_input("Enter population size")
+POP_SIZE = 500
+#POP_SIZE = st.number_input("Enter population size")
 
 #MUT_RATE: Rate at which our string will be changed.
 #MUT_RATE = 0.2
-MUT_RATE = st.number_input("Enter your mutation rate")
+MUT_RATE = st.text_input("Enter your mutation rate")
 
 #TARGET: Our goal.
 #TARGET = 'fakhitah'
